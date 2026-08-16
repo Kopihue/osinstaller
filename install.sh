bea8d86c7f9988a12096f28903e69a22f1a3a598
Kopihue <258140777+Kopihue@users.noreply.github.com>
2026-08-16 03:12:14 -0400

    almost done

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 clear
-echo -e "=== Setting all up for you, boss. ==="
-echo -e "=== Get ready... ==="
-sleep 1
 echo -e "oil..."
 sleep 1
 echo -e "up..."
@@ -20,4 +17,32 @@ paru -Syu --noconfirm
 
 cd $HOME
 
+paru -S --noconfirm linux-headers intel-ucode nvidia-dkms nvidia-utils sof-firmware
+paru -S --noconfirm pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber bluez bluez-utils
+paru -S --noconform noto-fonts noto-fonts-emoji nerd-fonts ttf-lilex
+paru -S --noconfirm hyprland sddm hyprpolkitagent alacritty hyprpaper mako wofi ranger xdg-desktop-portal-hyprland waybar wl-clipboard
+paru -S --noconfirm python-pipx tree-sitter-cli neovim gcc clang ripgrep
+paru -S --noconfirm openssh wireshark nmap arp-scan macchanger netcat
+pipx install kuky
+pipx ensurepath
+
+cd $HOME/osinstaller/
+mkdir ~/.config
+mv kuky/ ~/.config/
+
+sudo systemctl enable sddm bluetooth
+systemctl enable --user pipewire pipewire-pulse wireplumber
+
+sudo pacman -S --needed \
+	 qemu-desktop libvirt virt-manager virt-viewer \
+	 edk2-ovmf dnsmasq iptables-nft \
+	 swtpm dmidecode
+
+sudo systemctl enable --now libvirtd.service
+sudo usermod -aG libvirt,kvm $USER
+
+rm -rf ~/osinstaller/
+
+kuky switch pellet
+echo "REBOOT NOW"
 
